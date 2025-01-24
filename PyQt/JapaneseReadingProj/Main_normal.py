effa23eaba992212b06ce5fa3064ef01227a5249
LikeACodingStone <liszhouyou@gmail.com>
2025-01-24 03:26:56 +0000

    update japanese learning tools

--- PyQt/JapaneseReadingProj/Main_normal.py
+++ PyQt/JapaneseReadingProj/Main_normal.py
@@ -3,7 +3,7 @@ from fileinput import close
 import struct
 import base64
 import json
-import os,sys, ctypes
+import os,sys
 import re, time
 import threading
 import random
@@ -14,7 +14,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QBu
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import shutil
-from Ui_ReadJapanese import Ui_MainWindow
+from ReadJapanese_ui import Ui_MainWindow
 from PyTTSInterface import PyTTSInterface
 
 #pyinstaller -F -w -i kent.ico  SyncTwoDiskFiles.py	
@@ -22,53 +22,20 @@ from PyTTSInterface import PyTTSInterface
 deleteArray = []
 global btnChineMode
 dictCont = {}
-
-# 使用 pyttsx3
-# import pyttsx3
-import asyncio
-import edge_tts
-
-class JapaneseTTS:
-    def __init__(self):
-        self.voice = "ja-JP-NanamiNeural"
-        self.rate = "+0%"
-        self.volume = "+0%"
-        
-    async def _play_audio(self, text):
-        try:
-            communicate = edge_tts.Communicate(text, self.voice, rate=self.rate, volume=self.volume)
-            await communicate.save("temp.mp3")
-            
-            import pygame
-            pygame.mixer.init()
-            if os.path.exists("temp.mp3"):
-                pygame.mixer.music.load("temp.mp3")
-                pygame.mixer.music.play()
-                while pygame.mixer.music.get_busy():
-                    pygame.time.Clock().tick(10)
-                pygame.mixer.music.unload()
-                # 清理临时文件
-                os.remove("temp.mp3")
-        except Exception as e:
-            print(f"TTS Error: {str(e)}")
-            
-    def playAudioCont(self, text):
-        # 使用事件循环运行异步函数
-        asyncio.get_event_loop().run_until_complete(self._play_audio(text))
-
-jpTTS = JapaneseTTS()
+jpTTS = PyTTSInterface("Japanese", 100, 1.0)
 
 def ReadFunc(file):
     with open(file, "r", encoding='utf-8') as fp:
         contentLines = fp.readlines()
         lineLen = len(contentLines)
-        index = 0
-        while index < lineLen - 1:  # 修改循环逻辑
-            contentPre = contentLines[index].strip()
-            contentNext = contentLines[index + 1].strip()
-            if len(contentPre) > 1 and len(contentNext) > 1:
-                dictCont[contentPre] = contentNext
-            index += 2  # 每次跳过两行
+        for index in range(lineLen):
+            if index == lineLen -1:
+                pass
+            else:
+                contentPre = contentLines[index]
+                contentNext = contentLines[index+1]
+                if len(contentPre) > 1 and len(contentNext) > 1:
+                    dictCont[contentPre] = contentNext
         fp.close()
 
 keyArray = []
@@ -97,15 +64,9 @@ def Reflesh(ui):
     global selectId
     global keyArray
     global contArray
-    
-    # 生成0到pageSize-1的数字列表
-    nums = list(range(0, pageSize))
-    # 随机选择6个不重复的数字
-    randomNums = random.sample(nums, 6)
-    # 从这6个数字中选择一个作为正确答案
-    selectId = random.randint(0, 5)  # 0-5之间随机选择一个按钮位置
-    selectNum = randomNums[selectId]  # 使用选中的按钮位置对应的随机数
-    
+    nums = list(range(1, pageSize))
+    randomNums = random.sample(nums, 18)
+    selectNum = random.choice(randomNums)
     showBtnArray = []
     showTextArray = []
     if not btnChineMode:
@@ -114,32 +75,45 @@ def Reflesh(ui):
     else:
         showBtnArray = contArray
         showTextArray = keyArray
-        
-    # 设置按钮文本
     ui.pushButton_1.setText(showBtnArray[randomNums[0]])
     ui.pushButton_2.setText(showBtnArray[randomNums[1]])
     ui.pushButton_3.setText(showBtnArray[randomNums[2]])
     ui.pushButton_4.setText(showBtnArray[randomNums[3]])
     ui.pushButton_5.setText(showBtnArray[randomNums[4]])
     ui.pushButton_6.setText(showBtnArray[randomNums[5]])
-    
-    # selectId需要加1以匹配buttonGroup的ID（它们是从1开始的）
-    selectId += 1
-    
+    ui.pushButton_7.setText(showBtnArray[randomNums[6]])
+    ui.pushButton_8.setText(showBtnArray[randomNums[7]])
+    ui.pushButton_9.setText(showBtnArray[randomNums[8]])
+    ui.pushButton_10.setText(showBtnArray[randomNums[9]])
+    ui.pushButton_11.setText(showBtnArray[randomNums[10]])
+    ui.pushButton_12.setText(showBtnArray[randomNums[11]])
+    ui.pushButton_13.setText(showBtnArray[randomNums[12]])
+    ui.pushButton_14.setText(showBtnArray[randomNums[13]])
+    ui.pushButton_15.setText(showBtnArray[randomNums[14]])
+    ui.pushButton_16.setText(showBtnArray[randomNums[15]])
+    ui.pushButton_17.setText(showBtnArray[randomNums[16]])
+    ui.pushButton_18.setText(showBtnArray[randomNums[17]])
+    for index in range(18):
+        if randomNums[index] == selectNum:
+            selectId = index + 1
+            break
+        
+    ui.plainTextEdit.clear()
+    font = QFont("MS Gothic")
+    ui.plainTextEdit.setFont(font)
+    ui.plainTextEdit.setPlainText(showTextArray[selectNum])
     if btnChineMode:
         ttsText = ""
         if "|" in showTextArray[selectNum]:
             ttsText = showTextArray[selectNum].split("|")[1]
         else: 
             ttsText = showTextArray[selectNum]
-        replaceList = ["③", "◎", "①", "②", "④"]
+        replaceList = ["③", "◎", "①", "②"]
         for trRp in replaceList:
             ttsText = ttsText.replace(trRp,"")
-        ttsText = preprocessJapaneseText(ttsText)
-        try:
-            jpTTS.playAudioCont(ttsText)
-        except Exception as e:
-            print(f"Error playing audio: {str(e)}")
+        jpTTS.playAudioCont(ttsText)
+    styleGrey = "background-color: grey; font-size: 24px;text-align: center; padding-top: 50px; padding-bottom: 50px;"
+    ui.btnNext.setStyleSheet(styleGrey)
 
 global buttonGroup
 global tmPrevious
@@ -149,26 +123,14 @@ def PyQtBtnClicked(button):
     global selectId 
 
     btnId = buttonGroup.id(button)
-    styleRed = "background-color: red; font-size: 15px;text-align: center; padding-top: 50px; padding-bottom: 50px;"
-    styleCor = "background-color: green; font-size: 15px;text-align: center; padding-top: 50px; padding-bottom: 50px;"
+    styleRed = "background-color: red; font-size: 24px;text-align: center; padding-top: 50px; padding-bottom: 50px;"
+    styleCor = "background-color: green; font-size: 24px;text-align: center; padding-top: 50px; padding-bottom: 50px;"
     
     if btnId == selectId:
-        global selectNum 
-        global keyArray
-        global contArray
-        showBtnArray = []
-        showTextArray = []
-        if not btnChineMode:
-            showBtnArray = keyArray
-            showTextArray = contArray
-        else:
-            showBtnArray = contArray
-            showTextArray = keyArray
-        print(showTextArray[selectNum])
+        ui.btnNext.setStyleSheet(styleCor)
         Reflesh(ui)
-        pass
     else:
-        pass
+        ui.btnNext.setStyleSheet(styleRed)
    
 @pyqtSlot()
 def PyQtBtnPressed(id):
@@ -189,13 +151,16 @@ def ShowModeChangeSlot():
     global btnChineMode
     if btnChineMode == False:
         btnChineMode = True
+        ui.btnType.setText("Japanese")
     else:
         btnChineMode = False
+        ui.btnType.setText("Chinese")
 
 @pyqtSlot()
 def TimerLabelSlot():
     curTime = datetime.now()
     tmString = curTime.strftime("%Y-%m-%d %H:%M:%S")
+    ui.label.setText(tmString)
 
 @pyqtSlot()
 def DeleteText():
@@ -203,6 +168,9 @@ def DeleteText():
     tmString = curTime.strftime("%Y_%m_%d_%H_%M")
     if len(deleteArray) == 0:
         return
+    # currentPath = os.path.dirname(os.path.abspath(__file__))
+    # japanBackFile = currentPath + os.sep + "backFiles" + os.sep + "backList.txt"
+    # japaneseFile = currentPath + os.sep + "backFiles" + os.sep + "Japanese.txt"
     japaneseFile = "C:\\worksrc\\VSCODE_PROJ\\PythonCode\\JapaneseReadingProj\\backFiles\\Japanese.txt"
     japanBackFile = "C:\\worksrc\\VSCODE_PROJ\\PythonCode\\JapaneseReadingProj\\backFiles\\backList.txt"
     contentLines = []
@@ -227,6 +195,9 @@ def DeleteText():
         skipLines.append(delLine)
         skipLines.append(delLine + 1)
         skipLines.append(delLine + 2)
+        # del contentLines[delLine]
+        # del contentLines[delLine + 1]
+        # del contentLines[delLine + 2]
 
     newContent = []
     for index in range(contLen):
@@ -245,65 +216,43 @@ def DeleteText():
         for cont in newContent:
             fp.write(cont)
         fp.close()
+    # with open("D:\\ZhouyouGithub\\GitLab_Jimi\\PyQt\\JapaneseReadingProj\\backLog\\" + \
+    #             tmString + ".txt", "w", encoding='utf-8') as fp:
+    #     for array in deleteArray:
+    #         fp.write(array)
+    #         fp.write("\n")
+    #     fp.close()
 
-# 在播放前对文本进行预处理
-def preprocessJapaneseText(text):
-    # 添加适当的停顿
-    text = text.replace('、', '、 ')
-    text = text.replace('。', '。 ')
-    # 确保片假名和汉字之间有适当的间隔
-    text = re.sub(r'([ァ-ン])([\u4e00-\u9fff])', r'\1 \2', text)
-    return text
+@pyqtSlot()
+def ShowAnswer():
+    global dictCont
+    text = ui.plainTextEdit.toPlainText()
+    showText = ""
+    for key,value in dictCont.items():
+        if text in key:
+            showText = value
+            break
+        if text in value:
+            showText = key
+            break
+
+    msgBox = QMessageBox()
+    msgBox.setText(showText)
+    msgBox.setStandardButtons(QMessageBox.Ok)
+    msgBox.buttonClicked.connect(QMessageBox.close)
+    msgBox.exec_()
 
 if __name__ == '__main__':
-    if sys.platform == "win32":
-        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+    # currentPath = os.getcwd() 
+    # japaneseFile = currentPath + os.sep + "backFiles" + os.sep + "Japanese.txt"
     japaneseFile = "C:\\worksrc\\VSCODE_PROJ\\PythonCode\\JapaneseReadingProj\\backFiles\\Japanese.txt"
     ChangeContext(japaneseFile)
- 
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    
-    # 修改这里的窗口标志设置
-    MainWindow.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-    MainWindow.setAttribute(Qt.WA_TranslucentBackground)
-    
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-
-    # 添加一个关闭按钮（可选）
-    closeButton = QPushButton("×", MainWindow)
-    closeButton.setGeometry(MainWindow.width() - 30, 0, 30, 30)
-    closeButton.clicked.connect(MainWindow.close)
-    closeButton.setStyleSheet("""
-        QPushButton {
-            background-color: transparent;
-            color: black;
-            font-size: 16px;
-            border: none;
-        }
-        QPushButton:hover {
-            background-color: red;
-            color: white;
-        }
-    """)
-
-    # 添加窗口拖动功能
-    class MainWindowDraggable(QtWidgets.QMainWindow):
-        def mousePressEvent(self, event):
-            if event.button() == Qt.LeftButton:
-                self.dragPosition = event.globalPos() - self.frameGeometry().topLeft()
-                event.accept()
-
-        def mouseMoveEvent(self, event):
-            if event.buttons() == Qt.LeftButton:
-                self.move(event.globalPos() - self.dragPosition)
-                event.accept()
-
-    # 应用拖动功能
-    MainWindow.__class__ = MainWindowDraggable
-
-    btnChineMode = True
+    btnChineMode = False
     font = QFont("SimHei")
     ui.pushButton_1.setFont(font)
     ui.pushButton_2.setFont(font)
@@ -311,15 +260,42 @@ if __name__ == '__main__':
     ui.pushButton_4.setFont(font)
     ui.pushButton_5.setFont(font)
     ui.pushButton_6.setFont(font)
-
-    style = "color: rgb(0,0,139); font-size: 10px;text-align: left; padding-top: 50px; padding-bottom: 50px;"
+    ui.pushButton_7.setFont(font)
+    ui.pushButton_8.setFont(font)
+    ui.pushButton_9.setFont(font)
+    ui.pushButton_10.setFont(font)
+    ui.pushButton_11.setFont(font)
+    ui.pushButton_12.setFont(font)
+    ui.pushButton_13.setFont(font)
+    ui.pushButton_14.setFont(font)
+    ui.pushButton_15.setFont(font)
+    ui.pushButton_16.setFont(font)
+    ui.pushButton_17.setFont(font)
+    ui.pushButton_18.setFont(font)
+    style = "color: rgb(0,0,139); font-size: 12px;text-align: left; padding-top: 50px; padding-bottom: 50px;"
+    ui.plainTextEdit.setStyleSheet("font-size: 16px;text-align: center")
+    ui.textBrowser.setStyleSheet("font-size: 13px; color: rgb(0,0,139)")
+    ui.btnType.setStyleSheet("font-size: 12px;text-align: center")
+    ui.btnDelete.setStyleSheet("font-size: 16px;text-align: center")
+    ui.btnAns.setStyleSheet("font-size: 16px;text-align: center")
     ui.pushButton_1.setStyleSheet(style)
     ui.pushButton_2.setStyleSheet(style) 
     ui.pushButton_3.setStyleSheet(style) 
     ui.pushButton_4.setStyleSheet(style) 
     ui.pushButton_5.setStyleSheet(style) 
     ui.pushButton_6.setStyleSheet(style) 
-
+    ui.pushButton_7.setStyleSheet(style) 
+    ui.pushButton_8.setStyleSheet(style) 
+    ui.pushButton_9.setStyleSheet(style) 
+    ui.pushButton_10.setStyleSheet(style)
+    ui.pushButton_11.setStyleSheet(style)
+    ui.pushButton_12.setStyleSheet(style)
+    ui.pushButton_13.setStyleSheet(style)
+    ui.pushButton_14.setStyleSheet(style)
+    ui.pushButton_15.setStyleSheet(style)
+    ui.pushButton_16.setStyleSheet(style)
+    ui.pushButton_17.setStyleSheet(style)
+    ui.pushButton_18.setStyleSheet(style)
     buttonGroup = QButtonGroup()
     buttonGroup.addButton(ui.pushButton_1, 1)
     buttonGroup.addButton(ui.pushButton_2, 2)
@@ -327,9 +303,24 @@ if __name__ == '__main__':
     buttonGroup.addButton(ui.pushButton_4, 4)
     buttonGroup.addButton(ui.pushButton_5, 5)
     buttonGroup.addButton(ui.pushButton_6, 6)
+    buttonGroup.addButton(ui.pushButton_7, 7)
+    buttonGroup.addButton(ui.pushButton_8, 8)
+    buttonGroup.addButton(ui.pushButton_9, 9)
+    buttonGroup.addButton(ui.pushButton_10, 10)
+    buttonGroup.addButton(ui.pushButton_11, 11)
+    buttonGroup.addButton(ui.pushButton_12, 12)
+    buttonGroup.addButton(ui.pushButton_13, 13)
+    buttonGroup.addButton(ui.pushButton_14, 14)
+    buttonGroup.addButton(ui.pushButton_15, 15)
+    buttonGroup.addButton(ui.pushButton_16, 16)
+    buttonGroup.addButton(ui.pushButton_17, 17)
+    buttonGroup.addButton(ui.pushButton_18, 18)
     buttonGroup.buttonClicked.connect(PyQtBtnClicked)
     buttonGroup.buttonReleased.connect(PyQtBtnReleased)
     buttonGroup.buttonPressed.connect(PyQtBtnPressed)
+    ui.btnType.clicked.connect(ShowModeChangeSlot)
+    ui.btnDelete.clicked.connect(DeleteText)
+    ui.btnAns.clicked.connect(ShowAnswer)
     timer = QTimer()
     timer.timeout.connect(TimerLabelSlot)
     timer.start(100)
